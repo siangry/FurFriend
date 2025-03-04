7bf294d1a3292426f9010d43441a68025e6175fc
siangry <ruoying23@gmail.com>
2025-03-04 21:31:50 +0800

    feat: bottom navigation bat

--- app/src/main/java/com/example/furfriend/MainActivity.java
+++ app/src/main/java/com/example/furfriend/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.furfriend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.furfriend.databinding.ActivityMainBinding;
+import com.example.furfriend.screen.CalendarPage;
+import com.example.furfriend.screen.HomePage;
+import com.example.furfriend.screen.ProfilePage;
+import com.example.furfriend.screen.SearchPage;
+import com.example.furfriend.screen.SocialPage;
 
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomePage());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_home){
+                replaceFragment(new HomePage());
+            } else if (item.getItemId() == R.id.nav_search) {
+                replaceFragment(new SearchPage());
+            } else if (item.getItemId() == R.id.nav_social) {
+                replaceFragment(new SocialPage());
+            } else if (item.getItemId() == R.id.nav_calendar) {
+                replaceFragment(new CalendarPage());
+            } else if (item.getItemId() == R.id.nav_profile) {
+                replaceFragment(new ProfilePage());
+            }
+            return true;
+        });
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
---
+++ app/src/main/java/com/example/furfriend/screen/SocialPage.java
@@ -0,0 +1,20 @@
+package com.example.furfriend.screen;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.furfriend.R;
+
+public class SocialPage extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        return inflater.inflate(R.layout.screen_social_page, container, false);
+    }
+}
