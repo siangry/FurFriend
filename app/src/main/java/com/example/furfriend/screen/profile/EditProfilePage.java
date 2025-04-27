431e8ec7a6421b079efb7734de2743220c280063
siangry <ruoying23@gmail.com>
2025-04-27 16:03:44 +0800

    feat/ruoying: profile main page

---
+++ app/src/main/java/com/example/furfriend/screen/profile/EditProfilePage.java
@@ -0,0 +1,16 @@
+package com.example.furfriend.screen.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.furfriend.R;
+
+public class EditProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile_page);
+    }
+}
---
+++ app/src/main/java/com/example/furfriend/screen/profile/ViewAllPetPage.java
@@ -0,0 +1,16 @@
+package com.example.furfriend.screen.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.furfriend.R;
+
+public class ViewAllPetPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all_pet_page);
+    }
+}
